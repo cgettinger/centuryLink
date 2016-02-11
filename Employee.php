66a4452c92c2d18da22ae07ed8e0fbed186012c3
cgettinger <carl.gettinger@gmail.com>
2016-02-11 13:28:51 -0600

    Create Employee.php

---
+++ Employee.php
@@ -0,0 +1,14 @@
+<?php
+namespace centuryLink;
+/**
+ * Created by PhpStorm.
+ * User: Carl Gettinger
+ * Date: 2/8/2016
+ * Time: 7:52 PM
+ */
+class Employee
+{
+    const TYPE_MANAGER = 'MANAGER';
+    const TYPE_DEVELOPER = 'DEVELOPER';
+    const TYPE_QA_TESTER = 'QA TESTER';
+}
